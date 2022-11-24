c45e25d0244451ff7326333a8fb7eefcdd64b8cf
何朝城 <hi@hechaocheng.com>
2022-11-24 13:45:11 +0800

    Add files via upload

---
+++ lazyimgs.js
@@ -0,0 +1,22 @@
+/**************************************************
+* auther: hechaocheng(Hi[at]hechaocheng[dot]com)  *
+* github: https://github.com/hechaocheng/lazyimgs *
+**************************************************/
+"use strict";
+(function(w,d){
+	"use strict";
+	var imgs = d.getElementsByTagName("img");
+	var fn = function() {
+		if ( typeof(imgs) === "object" ){
+			for ( var i = 0, n = imgs.length; i < n; i++ ) {
+				if ( imgs[i].offsetTop < ((w.innerHeight||d.documentElement.clientHeight) + (w.scrollY||d.documentElement.scrollTop) ) && typeof(imgs[i].getAttribute("data-src")) === "string" ) {
+					if ( imgs[i].getAttribute("data-src") != imgs[i].getAttribute("src") ) {
+						imgs[i].setAttribute("src",imgs[i].getAttribute("data-src").length > 0 ? imgs[i].getAttribute("data-src") : imgs[i].getAttribute("src"));
+					}
+				}
+			}
+		}
+	};
+	fn();
+	w.onscroll = fn;
+}(window,document));
